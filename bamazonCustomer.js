ec091495fd2ccbb356dc6ef35fdf59140266572c
Tamami Polk <tamami_polk@hotmail.com>
2019-05-29 20:13:33 -0500

    Challenge One Completed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,11 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// global variables
+
+
+var userItemId;
+var userCount;
 
 var connection = mysql.createConnection( {
     host: "localhost",
@@ -16,12 +23,63 @@ connection.connect(function(err) {
 });
 
 function showInventory() {
-    connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
+    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function(err, res) {
         if (err) throw err;
         console.table(res);
-        connection.end();
+        // connection.end();
+        getUserInput();
     })
 };
+
+
+function getUserInput() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "product_id_input",
+            message: "What is the ID of the product you would like to buy?"
+        },
+        {
+            type: "input",
+            name: "count_input",
+            message: "How many would you like to purchase?"
+        }
+    ]).then(function (user) {
+        userItemId = user.product_id_input;
+        userCount = user.count_input;
+
+        processOrder(userItemId);
+
+
+    })
+}
+
+function processOrder(itemID) {
+
+    var theSelectQuery = mysql.format("SELECT * FROM products WHERE item_id = ?", [itemID]);
+
+    connection.query(theSelectQuery, function (err, res) {
+
+        var stockQuantity = res[0].stock_quantity - userCount;
+
+        if (stockQuantity > 0) {
+
+            var totalPrice = res[0].price * userCount;
+            console.log("Your Total Cost Is: $"+ totalPrice);
+
+            var theUpdateStatement = mysql.format("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [stockQuantity, itemID]);
+
+            connection.query(theUpdateStatement, function (err, res) {
+                console.log("The Inventory Has Been Updated!\n");
+            })
+        } else { console.log("there is not enough inventory")}
+
+        productName = res[0].product_name;
+        price = res[0].price;
+        departmentName = res[0].department_name;
+    });
+
+}
 
 
 
